32f809b4ce5a2d170e6f34a8fe6f770b4fb9e02a
sl3wang <sl3wang@uwaterloo.ca>
2020-05-16 01:13:30 -0400

    routing

--- RUHacks/src/app/app-routing.module.ts
+++ RUHacks/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatbotComponent } from './chatbot/chatbot.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'chatbot', component: ChatbotComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
